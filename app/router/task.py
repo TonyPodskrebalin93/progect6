bfdd8dc525acf4186207862ab19e5dd0ec8de020
TonyPodskrebalin93 <podskrebalin93@mail.ru>
2025-01-08 19:38:13 +0300

    1231412421213112124211231212312

--- app/router/task.py
+++ app/router/task.py
@@ -1,23 +1,17 @@
-from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 
-router = APIRouter(prefix='/task', tags=['task'])
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("tasks.id"), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
+    user = relationship("User", back_populates="tasks")
 
-@router.get("/")
-async def all_task():
-    pass
-
-@router.get("/task_id")
-async def task_by_id():
-    pass
-
-@router.post("/create")
-async def create_task():
-    pass
-
-@router.put("/update")
-async def update_task():
-    pass
-
-@router.delete("/delete")
-async def delete_task():
-    pass
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
